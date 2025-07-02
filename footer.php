9230d878c61754355f9d51171f909b9aad0810b5
jcd3dr <102407900+jcd3dr@users.noreply.github.com>
2025-07-02 17:56:01 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,16 @@
-    </div>
-</div>
-<footer id="colophon" class="site-footer" style="background-color: #111; padding: 40px 20px; text-align: center; color: #00e6c3;">
+    </div> <!-- Cierra #content -->
+
+    <!-- Pie de página -->
+    <footer id="colophon" class="site-footer">
         <div class="site-info">
-            <a href="<?php echo esc_url( __( 'https://dadecore.com/', 'dadecore-theme' ) ); ?>">
-                <?php
-                /* translators: %s: CMS name, i.e. WordPress. */
-                printf( esc_html__( 'Proudly powered by %s', 'dadecore-theme' ), 'Dadecore.com' ); ?>
+            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dadecore-theme' ) ); ?>">
+                <?php printf( esc_html__( 'Orgullosamente impulsado por %s', 'dadecore-theme' ), 'WordPress' ); ?>
             </a>
             <span class="sep"> | </span>
-            <?php
-            /* translators: 1: Theme name, 2: Theme author. */
-            printf( esc_html__( 'Theme: %1$s by %2$s.', 'dadecore-theme' ), 'DadeCore Theme', '<a href="https://www.dadecore.com/">DadeCore Bizz LLC</a>' );
-            ?>
-        </div></footer></div><?php wp_footer(); ?>
+            <?php printf( esc_html__( 'Tema: %1$s por %2$s.', 'dadecore-theme' ), 'DadeCore Theme', '<a href="https://dadecore.com/">DadeCore Bizz LLC</a>' ); ?>
+        </div>
+    </footer><!-- #colophon -->
 
+    <?php wp_footer(); ?>
 </body>
 </html>
